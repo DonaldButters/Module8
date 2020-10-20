3b028f4a72be11b1a7d9624575531ac264e9b455
DonaldButters <70549495+DonaldButters@users.noreply.github.com>
2020-10-19 20:53:28 -0500

    Add files via upload

--- more_fun_with_collections/set_membership.py
+++ more_fun_with_collections/set_membership.py
@@ -1,3 +1,19 @@
+"""
+
+Program: set_membership.py
+
+Author: Donald Butters
+
+Last date modified: 10/19/2020
+
+The purpose of this program is to accept a set
+and return a boolean value stating if the element is in the set
+
+peram1 :accept a set
+
+return :boolean value
+
+"""
 def in_set():
     set_one = set('123456789')
     x = input("Please enter a set of numbers between 1-10 :")
